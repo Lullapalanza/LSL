4a8d9a87f0f37a5f018d77f3e7cc7c2b67b8385d
jyrgenLaboris <jyrgen.luus@gmail.com>
2020-02-05 15:12:38 +0200

    1.0.0 Initial code

--- QE_measurements/QE_measurement.py
+++ QE_measurements/QE_measurement.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import json
 
 from win32gui import FindWindow
 from win32api import SendMessage
@@ -24,40 +25,44 @@ class Window(QMainWindow):
         grp_box0.setFixedSize(390, 290)
 
         # Connection to Mono
-        conn_text = QLabel("Mono program: ---------", grp_box0)
-        conn_text.move(10, 60)
-        conn_button = QPushButton("Connect to mono", grp_box0)
-        conn_button.resize(145, 27)
-        conn_button.clicked.connect(self.connect_to_mono)
-        conn_button.move(230, 55)
+        self.conn_text = QLabel("Mono program: ---------", grp_box0)
+        self.conn_text.move(10, 60)
+        self.conn_button = QPushButton("Connect to mono", grp_box0)
+        self.conn_button.resize(145, 27)
+        self.conn_button.clicked.connect(self.connect_to_mono)
+        self.conn_button.move(230, 55)
 
         # Connection to Sourcemeter
-        source_text = QLabel("Sourcemeter: ---------", grp_box0)
-        source_text.move(10, 110)
-        source_button = QPushButton("Connect to SM", grp_box0)
-        source_button.resize(145, 27)
-        source_button.clicked.connect(self.connect_to_SM)
-        source_button.move(230, 105)
+        self.source_text = QLabel("Sourcemeter: ---------", grp_box0)
+        self.source_text.move(10, 110)
+        self.source_button = QPushButton("Connect to SM", grp_box0)
+        self.source_button.resize(145, 27)
+        self.source_button.clicked.connect(self.connect_to_SM)
+        self.source_button.move(230, 105)
 
         # Set WL
-        wl_text = QLabel("Wavelength: 500 ", grp_box0)
-        wl_text.move(10, 160)
-        new_wl_text = QLineEdit(grp_box0)
-        new_wl_text.resize(50, 25)
-        new_wl_text.move(170, 155)
-        wl_button = QPushButton("Set wl", grp_box0)
-        wl_button.clicked.connect(self.change_wl)
-        wl_button.move(230, 155)
+        self.wl_text = QLabel("Wavelength: 500 ", grp_box0)
+        self.wl_text.move(10, 160)
+        self.new_wl_text = QLineEdit(grp_box0)
+        self.new_wl_text.resize(50, 25)
+        self.new_wl_text.move(170, 155)
+        self.wl_button = QPushButton("Set wl", grp_box0)
+        self.wl_button.clicked.connect(self.change_wl)
+        self.wl_button.move(230, 155)
 
         # Shutter
-        shutter_text = QLabel("Shutter: CLOSED", grp_box0)
-        shutter_text.move(10, 210)
-        shutter_open_button = QPushButton("Open", grp_box0)
-        shutter_open_button.clicked.connect(self.open_shutter)
-        shutter_open_button.move(150, 205)
-        shutter_closed_button = QPushButton("Closed", grp_box0)
-        shutter_closed_button.clicked.connect(self.close_shutter)
-        shutter_closed_button.move(230, 205)
+        self.shutter_text = QLabel("Shutter: CLOSED", grp_box0)
+        self.shutter_text.move(10, 210)
+        self.shutter_open_button = QPushButton("Open", grp_box0)
+        self.shutter_open_button.clicked.connect(self.open_shutter)
+        self.shutter_open_button.move(150, 205)
+        self.shutter_closed_button = QPushButton("Closed", grp_box0)
+        self.shutter_closed_button.clicked.connect(self.close_shutter)
+        self.shutter_closed_button.move(230, 205)
+        
+        self.test_button = QPushButton("Test Keithley", grp_box0)
+        self.test_button.move(10, 255)
+        self.test_button.clicked.connect(self.test_SM)
 
         return grp_box0
     
@@ -66,50 +71,56 @@ class Window(QMainWindow):
         grp_box1.setFixedSize(390, 290)
 
         # Start WL
-        start_wl_Label = QLabel("Start wl: 500 ", grp_box1)
-        start_wl_Label.move(10, 60)
+        self.start_wl_Label = QLabel("Start wl: 500 ", grp_box1)
+        self.start_wl_Label.move(10, 60)
 
-        start_wl_LineEdit = QLineEdit(grp_box1)
-        start_wl_LineEdit.resize(50, 25)
-        start_wl_LineEdit.move(160, 55)
+        self.start_wl_LineEdit = QLineEdit(grp_box1)
+        self.start_wl_LineEdit.resize(50, 25)
+        self.start_wl_LineEdit.move(160, 55)
 
-        start_wl_Button = QPushButton("Set start wl", grp_box1)
-        start_wl_Button.resize(130, 27)
-        start_wl_Button.clicked.connect(self.set_start_wl)
-        start_wl_Button.move(220, 55)
+        self.start_wl_Button = QPushButton("Set start wl", grp_box1)
+        self.start_wl_Button.resize(130, 27)
+        self.start_wl_Button.clicked.connect(self.set_start_wl)
+        self.start_wl_Button.move(220, 55)
 
         # End Wl
-        end_wl_Label = QLabel("End wl: 1050", grp_box1)
-        end_wl_Label.move(10, 110)
+        self.end_wl_Label = QLabel("End wl: 1050", grp_box1)
+        self.end_wl_Label.move(10, 110)
 
-        end_wl_LineEdit = QLineEdit(grp_box1)
-        end_wl_LineEdit.resize(50, 25)
-        end_wl_LineEdit.move(160, 105)
+        self.end_wl_LineEdit = QLineEdit(grp_box1)
+        self.end_wl_LineEdit.resize(50, 25)
+        self.end_wl_LineEdit.move(160, 105)
 
-        end_wl_Button = QPushButton("Set end wl", grp_box1)
-        end_wl_Button.resize(130, 27)
-        end_wl_Button.clicked.connect(self.set_end_wl)
-        end_wl_Button.move(220, 105)
+        self.end_wl_Button = QPushButton("Set end wl", grp_box1)
+        self.end_wl_Button.resize(130, 27)
+        self.end_wl_Button.clicked.connect(self.set_end_wl)
+        self.end_wl_Button.move(220, 105)
 
         # Step
-        step_Label = QLabel("Step: 10 ", grp_box1)
-        step_Label.move(10, 160)
+        self.step_Label = QLabel("Step: 10 ", grp_box1)
+        self.step_Label.move(10, 160)
 
-        step_LineEdit = QLineEdit(grp_box1)
-        step_LineEdit.resize(50, 25)
-        step_LineEdit.move(160, 155)
+        self.step_LineEdit = QLineEdit(grp_box1)
+        self.step_LineEdit.resize(50, 25)
+        self.step_LineEdit.move(160, 155)
 
-        step_Button = QPushButton("Set step", grp_box1)
-        step_Button.resize(130, 27)
-        step_Button.clicked.connect(self.set_step)
-        step_Button.move(220, 155)
+        self.step_Button = QPushButton("Set step", grp_box1)
+        self.step_Button.resize(130, 27)
+        self.step_Button.clicked.connect(self.set_step)
+        self.step_Button.move(220, 155)
 
-        start_Button = QPushButton("Start", grp_box1)
-        start_Button.clicked.connect(self.start_measurement)
-        start_Button.resize(130, 27)
-        start_Button.move(220, 210)
+        self.start_Button = QPushButton("Start", grp_box1)
+        self.start_Button.clicked.connect(self.start_measurement)
+        self.start_Button.resize(130, 27)
+        self.start_Button.move(220, 210)
         
         return grp_box1
+        
+    def test_SM(self):
+        self.VA_wls = [400, 500, 600, 700, 800, 900, 1000]
+        if self.keithley:
+            self.keithley.write("*TST?")
+            print(self.keithley.read())
 
     def setup_graph(self):
         self.figure = plt.figure()
@@ -162,34 +173,43 @@ class Window(QMainWindow):
         if self.hwnd != 0:
             self.connected = True
             self.conn_text.setText("Mono program: connected")
-            SendMessage(self.hwnd, self.WM_SET_WAVE, 500, 0)
-            SendMessage(self.hwnd, self.WM_SET_SHUTTER, 0, 0)
+            SendMessage(self.hwnd, WM_SET_WAVE, 500, 0)
+            SendMessage(self.hwnd, WM_SET_SHUTTER, 0, 0)
         else:
             print("Didn't connect")
 
     def connect_to_SM(self):
-        if self.connected:
-            self.keithley = Instrument()
-            self.source_text.setText("Sourcemeter: connected")
+        # if self.connected:
+        self.keithley = Instrument.Instrument()
+        print("setting up")
+        self.keithley.setup(arm=0.1, arm_counts=1, trig_counts=1)
+        print("source mode change")
+        self.keithley.source_mode("V")
+        print("measure current")
+        self.keithley.measure_current()
+        print("comp current")
+        self.keithley.set_compliance_current(current=0.003)
+        print("source val")
+        self.source_text.setText("Sourcemeter: connected")
 
     def change_wl(self):
         if self.connected:
             try:
                 self.new_wl = int(self.new_wl_text.text())
                 if 100 < self.new_wl < 2000:
-                    SendMessage(self.hwnd, self.WM_SET_WAVE, self.new_wl, 0)
+                    SendMessage(self.hwnd, WM_SET_WAVE, self.new_wl, 0)
                     self.wl_text.setText("Wavelength: " + str(self.new_wl))
             except ValueError:
                 print("Please enter an int")
 
     def open_shutter(self):
         if self.connected:
-            SendMessage(self.hwnd, self.WM_SET_SHUTTER, 1, 0)
+            SendMessage(self.hwnd, WM_SET_SHUTTER, 1, 0)
             self.shutter_text.setText("Shutter:  OPEN ")
 
     def close_shutter(self):
         if self.connected:
-            SendMessage(self.hwnd, self.WM_SET_SHUTTER, 0, 0)
+            SendMessage(self.hwnd, WM_SET_SHUTTER, 0, 0)
             self.shutter_text.setText("Shutter: CLOSED")
 
     def reset(self):
@@ -215,71 +235,77 @@ class Window(QMainWindow):
         print("Hello")
 
     def measure_VA(self, wl):
-        ser = serial.Serial("COM7", BAUD, timeout=2)
-        if ser.isOpen():
-            ser.write("*TST?".encode("ascii"))
-            out = ser.read()
-            print(out)
-            ser.close()
-            # exit()
-        # try:
-        # self.keithley.apply_voltage()
-        # sleep(0.5)
-        # self.keithley.source_voltage_range = 1e-2
-        # sleep(0.5)
-        # self.keithley.compliance_current = 0.1
-        # sleep(0.5)
-        # self.keithley.source_voltage = 0
-        # sleep(0.5)
-        # self.keithley.enable_source()
-        # print("1/2 of setup for VA")
-        # sleep(1)
-
-        # self.keithley.measure_current()
-        # sleep(1)
-        # self.keithley.sample_continuously()
-        # sleep(1)
-        # self.keithley.set_timed_arm(0.1)
-        # print("VA setup done")
-        # sleep(1)
-
-        # self.keithley.ramp_to_voltage(0.01)
-        # sleep(1)
-        # print(self.keithley.current)
-        # sleep(1)
-        # self.keithley.ramp_to_voltage(0.02)
-        # sleep(1)
-        # print(self.keithley.current)
-        # sleep(1)
-        # self.keithley.ramp_to_voltage(0.03)
-        # sleep(1)
-        # print(self.keithley.current)
-        # sleep(1)
-        # print(self.keithley.error)
-        # finally:
-        # self.keithley.shutdown()
+        self.VA_wls.remove(wl)
+        volt_data = []
+        curr_data = []
+        self.keithley.source_value(0)
+        sleep(5)
+        temp_val = self.keithley.get_measurement()
+        V = 0
+        while temp_val < 0.003 and V <= 1.5:
+            self.keithley.source_value(V)
+            sleep(1)
+            temp_val = self.keithley.get_measurement()
+            sleep(1)
+            print(f"Measuring VA {temp_val}")
+            volt_data.append(V)
+            curr_data.append(temp_val)
+            V += 0.1
+            
+        self.keithley.source_value(0)
+        sleep(5)
+        temp_val = self.keithley.get_measurement()
+        V = 0
+        while temp_val > -10**(-5) and V >= -2:
+            self.keithley.source_value(V)
+            sleep(1)
+            temp_val = self.keithley.get_measurement()
+            sleep(1)
+            print(f"Measuring VA {temp_val}")
+            volt_data.append(V)
+            curr_data.append(temp_val)
+            V -= 0.1
+            
+        self.keithley.source_value(0)
+        sleep(5)
+        self.content.update({wl: [volt_data, curr_data]})
 
     def start_measurement(self):
-
         if self.test:
             self.data = []
             self.wls = []
             for i in range((self.end_wl - self.start_wl) // self.step + 1):
                 self.wls.append(self.start_wl + self.step * i)
+             
+             
+            print("setting up")
+            self.keithley.setup()
+            print("source mode change")
+            self.keithley.source_mode("V")
+            print("measure current")
+            self.keithley.measure_current()
+            print("comp current")
+            self.keithley.set_compliance_current()
+            print("source val")
+            self.keithley.source_value(0)
+            self.keithley.source_enabled(True)
+            sleep(5)
+            
             for wl in self.wls:
                 print(wl)
-
                 sleep(0.1)
                 if wl in self.VA_wls:
                     self.measure_VA(wl)
-                    sleep(5)
                 current = random.randint(0, 100) + wl
                 self.data.append(current)
                 self.subplot.clear()
                 self.subplot.plot(self.wls[0:len(self.data)], self.data)
                 self.canvas.draw()
                 self.canvas.flush_events()
-
+            
+            print(self.keithley.source_enabled(False))
+            self.content.update({"Spectrum": [self.wls, self.data]})
+            
         elif self.connected:
             self.data = []
             self.wls = []
@@ -287,46 +313,55 @@ class Window(QMainWindow):
                 self.wls.append(self.start_wl + self.step * i)
 
             try:
-                # self.keithley.apply_voltage()
-                # self.keithley.source_voltage_range = 1e-3
-                # self.keithley.compliance_voltage = 1
-                self.keithley.setup()
-                self.keithley.source_mode("V")
                 self.keithley.source_value(0)
                 self.keithley.source_enabled(True)
 
-                # self.keithley.measure_current()
-                # self.keithley.sample_continuously()
-                # self.keithley.set_timed_arm(0.1)
-
-                SendMessage(self.hwnd, self.WM_SET_SHUTTER, 1, 0)
-                SendMessage(self.hwnd, self.WM_SET_WAVE, self.start_wl, 0)
+                SendMessage(self.hwnd, WM_SET_SHUTTER, 1, 0)
+                SendMessage(self.hwnd, WM_SET_WAVE, self.start_wl, 0)
                 sleep(1)
 
                 for wl in self.wls:
                     print(wl)
-                    SendMessage(self.hwnd, self.WM_SET_WAVE, wl, 0)
+                    SendMessage(self.hwnd, WM_SET_WAVE, wl, 0)
                     sleep(0.1)
-                    current = self.keithley.current
+                    current = self.keithley.get_measurement()
                     self.data.append(current)
                     self.subplot.clear()
                     self.subplot.plot(self.wls[0:len(self.data)], self.data)
                     self.canvas.draw()
                     self.canvas.flush_events()
+                    
+                    if wl in self.VA_wls:
+                        self.measure_VA(wl)
             finally:
-                SendMessage(self.hwnd, self.WM_SET_SHUTTER, 0, 0)
-                self.keithley.shutdown()
+                SendMessage(self.hwnd, WM_SET_SHUTTER, 0, 0)
+                self.keithley.source_value(0)
+                self.keithley.source_enabled(False)
+                self.content.update({"Spectrum": [self.wls, self.data]})
 
     def save(self):
-        self.file_name = QFileDialog.getSaveFileName(self, 'Save File')
-        self.file = open(self.file_name[0], 'w')
-        for i in range(len(self.wls)):
-            self.file.write(str(self.wls[i]) + '\t' + str(self.data[i]) + '\n')
-        self.file.close()
-    #####
+        print(self.content)
+        file_name = QFileDialog.getSaveFileName(self, 'Save File')
+        if file_name != ('', ''):
+            file = open(f"{file_name[0]}.json", 'w')
+            json.dump(self.content, file)
+            file.close()
+        self.content = {}
+            
+    def closeEvent(self, event):
+        if self.keithley:
+            self.keithley.close()
+        print("Closing app")
 
     def __init__(self, parent=None):
-        self.VA_wls = [600, 1000]
+        self.VA_wls = []
+        self.connected = False
+        self.test = False
+        self.start_wl = 500
+        self.end_wl = 1050
+        self.step = 10
+        self.keithley = None
+        self.content = {}
 
         QWidget.__init__(self, parent=parent)
         self.initUI()
